db4c1382849917543b304f751cd51da1b8082105
ShinyaT <rm0063vpedc15@gmail.com>
2023-05-15 07:43:13 +0000

    add cloudfront and s3 resource

--- cdk_static_site_cf/cdk_static_site_cf_stack.py
+++ cdk_static_site_cf/cdk_static_site_cf_stack.py
@@ -1,19 +1,41 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_cloudfront as cf,
+    aws_cloudfront_origins as cf_origins,
+    aws_s3_deployment as s3deploy,
 )
 from constructs import Construct
 
+
 class CdkStaticSiteCfStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # S3 Bucket
+        bucket = s3.Bucket(
+            self, 
+            "MyStaticSiteBucket"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStaticSiteCfQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # CloudFront
+        distribution = cf.Distribution(
+            self,
+            'Distribution',
+            default_behavior=cf.BehaviorOptions(
+                origin=cf_origins.S3Origin(bucket)
+            ),
+            default_root_object='index.html',
+        )
+
+        # Deploy site contents to S3 Bucket
+        s3deploy.BucketDeployment(
+            self, 
+            "DeployWithInvalidation",
+            sources=[s3deploy.Source.asset("./website")],
+            destination_bucket=bucket,
+            distribution=distribution,
+            distribution_paths=["/*"]
+        )
+
